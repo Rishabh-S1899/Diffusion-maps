7192976d60917fdaaa31ef93679aaa2ec2b20167
Peter7Parker <ankitsharma30072001@gmail.com>
2026-02-08 01:49:58 +0530

    Added code for storing attention maps

--- demo/demo-sd3-5.py
+++ demo/demo-sd3-5.py
@@ -1,10 +1,13 @@
 import torch
 from diffusers import StableDiffusion3Pipeline
+import os
 from attention_map_diffusers import (
     attn_maps,
     init_pipeline,
     save_attention_maps
 )
+current_directory = os.getcwd()
+os.environ['HF_HOME'] = current_directory
 
 pipe = StableDiffusion3Pipeline.from_pretrained(
     "stabilityai/stable-diffusion-3.5-medium",
@@ -19,7 +22,7 @@ pipe = init_pipeline(pipe)
 # recommend not using batch operations for sd3, as cpu memory could be exceeded.
 prompts = [
     # "A photo of a puppy wearing a hat.",
-    "A capybara holding a sign that reads Hello World.",
+    "A red helicopter flying over a city skyline at sunset.",
 ]
 
 images = pipe(
@@ -28,9 +31,24 @@ images = pipe(
     guidance_scale=4.5,
 ).images
 
+print("This is the generated images:\n", images)
 for batch, image in enumerate(images):
     image.save(f'{batch}-sd3-5.png')
 
 ##### 2. Process and Save attention map #####
+
+# Save raw attention map tensors for later analysis
+import pickle
+raw_tensor_dir = 'attn_maps-sd3-5-tensors'
+os.makedirs(raw_tensor_dir, exist_ok=True)
+for timestep, layers in attn_maps.items():
+    timestep_dir = os.path.join(raw_tensor_dir, f'{timestep}')
+    os.makedirs(timestep_dir, exist_ok=True)
+    for layer, attn_map in layers.items():
+        tensor_path = os.path.join(timestep_dir, f'{layer}.pkl')
+        with open(tensor_path, 'wb') as f:
+            pickle.dump(attn_map.cpu(), f)
+
+# Save visual attention maps as before
 save_attention_maps(attn_maps, pipe.tokenizer, prompts, base_dir='attn_maps-sd3-5', unconditional=True)
 #############################################
